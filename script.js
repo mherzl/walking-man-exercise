5f0ca484a386964a814edc4b470afb8e8fff06b4
Matthew Herzl <matthew.herzl@colorado.edu>
2015-05-21 15:03:27 -0600

    completed the assignment

--- script.js
+++ script.js
@@ -7,9 +7,20 @@ man.style.position="absolute";
 man.style.left="0px";
 
 var manPos = 0;
+var advance = 1;
 var setManPos = function(e) {
-  manPos += 10;
+  if(manPos >= 400){
+    advance = -1;
+    man.style.transform="rotateY(180deg)";
+  }
+  if(manPos <= 0){
+    advance = 1;
+    man.style.transform="";
+  }
+  manPos += advance;
   man.style.left=manPos + "px";
 };
+
+var intervalID = window.setInterval(setManPos, 8);
 
 man.addEventListener('click',setManPos);
