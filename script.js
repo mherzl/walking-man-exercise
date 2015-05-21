d5849149e5c2bf2bb1a6f2ae8c8915f1552b4567
Matthew Herzl <matthew.herzl@colorado.edu>
2015-05-21 15:23:40 -0600

    man walks up and down

--- script.js
+++ script.js
@@ -6,19 +6,31 @@ man.style.left="0px";
 
 var manPos = 0;
 var advance = 1;
+var height = 0;
+var dropping = 50;
 var setManPos = function(e) {
-  if(manPos >= 900){
+  if(manPos >= 200/*900*/){
     advance = -1;
     man.style.transform="rotateY(180deg)";
+    height += dropping;
+    man.style.top=height + "px";
   }
-  if(manPos <= 0){
+  else if(manPos <= 0){
     advance = 1;
     man.style.transform="";
+    height += dropping;
+    man.style.top=height + "px";
+  }
+  if(height >= 500){
+    dropping = -50;
+  }
+  else if(height < 0){
+    dropping = 50;
   }
   manPos += advance;
   man.style.left=manPos + "px";
 };
 
-var intervalID = window.setInterval(setManPos, 8);
+var intervalID = window.setInterval(setManPos, 1/*8*/);
 
 man.addEventListener('click',setManPos);
